c284dee07038d1c8366d5c69884d3a55e9855759
Korzun Anton <akorzun.91@gmail.com>
2018-06-05 12:09:20 +0300

    Add view service

---
+++ WF.EnrolleeApplication.DataAccess/WF.EnrolleeApplication.DataAccess/Interfaces/IViewService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WF.EnrolleeApplication.DataAccess.EntityFramework;
+
+namespace WF.EnrolleeApplication.DataAccess.Interfaces
+{
+    interface IViewService
+    {     
+        List<EmployeeView> GetEmployees();
+        List<EnrolleeView> GetEnrollees();
+        List<SpecialityView> GetSpecialities();
+    }
+}
---
+++ WF.EnrolleeApplication.DataAccess/WF.EnrolleeApplication.DataAccess/Services/ViewService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WF.EnrolleeApplication.DataAccess.EntityFramework;
+using WF.EnrolleeApplication.DataAccess.Interfaces;
+
+namespace WF.EnrolleeApplication.DataAccess.Services
+{
+    public class ViewService : IViewService
+    {
+        private EnrolleeContext context;
+        public ViewService(string connectionString)
+        {
+            context = new EnrolleeContext(connectionString);
+        }
+        public List<EmployeeView> GetEmployees()
+        {
+            List<EmployeeView> employees = context.EmployeeView.ToList();
+            return employees;
+        }
+
+        public List<EnrolleeView> GetEnrollees()
+        {
+            List<EnrolleeView> enrollees = context.EnrolleeView.ToList();
+            return enrollees;
+        }
+
+        public List<SpecialityView> GetSpecialities()
+        {
+            List<SpecialityView> specialities = context.SpecialityView.ToList();
+            return specialities;
+        }
+    }
+}
