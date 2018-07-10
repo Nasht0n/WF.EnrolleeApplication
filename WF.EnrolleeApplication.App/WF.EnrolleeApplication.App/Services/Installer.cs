a8d9a3303f2401fed90272b1b226a96c315438d7
Anton Korzun <akorzun.91@gmail.com>
2018-07-10 11:47:27 +0300

    Installer

--- WF.EnrolleeApplication.App/WF.EnrolleeApplication.App/Services/Installer.cs
+++ WF.EnrolleeApplication.App/WF.EnrolleeApplication.App/Services/Installer.cs
@@ -4,10 +4,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
 using System.Configuration.Install;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using NLog;
+using NLog.Config;
 
 namespace WF.EnrolleeApplication.App.Services
 {
@@ -32,7 +35,7 @@ namespace WF.EnrolleeApplication.App.Services
             string userId = Context.Parameters["USERID"];
             string password = Context.Parameters["PASSWORD"];
             string connectionString = $"data source={dataSource};initial catalog={initialCatalog};persist security info=True;user id={userId};password={password};MultipleActiveResultSets=True;App=EntityFramework";
-
+            // Файл конфигурации проекта
             ExeConfigurationFileMap map = new ExeConfigurationFileMap();
             string configFile = string.Concat(Assembly.GetExecutingAssembly().Location, ".config");
             map.ExeConfigFilename = configFile;
@@ -40,6 +43,12 @@ namespace WF.EnrolleeApplication.App.Services
             config.ConnectionStrings.ConnectionStrings["EnrolleeContext"].ConnectionString = connectionString;
             config.Save(ConfigurationSaveMode.Full);
             ConfigurationManager.RefreshSection("connectionStrings");
+            // Файл конфигурации логирования
+            string assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = assemblyFolder + "\\NLog.config";
+            string configNlog = File.ReadAllText(path);
+            configNlog = configNlog.Replace("@Server", $"\\\\{dataSource}");
+            File.WriteAllText(path,configNlog);
         }
     }
 }
